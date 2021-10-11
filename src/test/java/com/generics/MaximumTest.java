8ea86e9945612729e433d6eb1dd61449eb4b3b89
MKS1998 <nksayyad2900@outlook.com>
2021-10-11 15:24:25 +0530

    Added : Refactor To Generic Method

---
+++ src/test/java/com/generics/MaximumTest.java
@@ -0,0 +1,25 @@
+package com.generics;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class MaximumTest {
+	@Test
+	public void intMax() {
+		int assertValue = Maximum.getmaximum(10, 20, 5);
+		Assert.assertEquals(20, assertValue);
+	}
+
+	public void floatMax() {
+		float assertValue = Maximum.getmaximum(10f, 20f, 5f);
+		Assert.assertEquals(20, assertValue, 0);
+	}
+
+	public void stringMax() {
+		String assertValue = Maximum.getmaximum("abc", "def", "ghi");
+		Assert.assertEquals("ghi", assertValue);
+	}
+}
